7ed0ab30985ded475234abd87646a0d8f44274e2
SotirisTsioutsiouliklis <sotostsiou2@hotmail.com>
2020-05-30 08:26:04 +0300

    add uniform start point to stochastik residual

--- Code/Cpp_files/residual_stochastic_opt.cpp
+++ Code/Cpp_files/residual_stochastic_opt.cpp
@@ -482,8 +482,8 @@ int main(int argc, char **argv) {
     int whole_iterations = 0;
 
     // Initialize start point.
-    current_point = algs.get_proportional_excess_vector();
-    //current_point = get_uniform_initial_point();
+    //current_point = algs.get_proportional_excess_vector();
+    current_point = get_uniform_initial_point();
     //current_point = get_random_initial_point();
 
     // Get custom LFPR.
