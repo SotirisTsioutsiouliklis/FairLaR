0782b64a22be2ec09c57f179eef35818451aee5d
SotirisTsioutsiouliklis <sotostsiou2@hotmail.com>
2020-09-04 10:01:03 +0200

    creating ll to one script

---
+++ Code/Python_files/adhock/allToOneFairLar.py
@@ -0,0 +1,4 @@
+import subprocess
+
+subprocess.call(['cp ~/Workspace/FairLar/Code/Cpp_files/pagerank.out .'], cwd = '.' , shell= True)
+subprocess.call(['cp ~/Workspace/FairLar/Code/Python_files/sensitive.py .'], cwd = '.' , shell= True)
